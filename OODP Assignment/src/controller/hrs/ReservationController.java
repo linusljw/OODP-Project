021fc08713422b821647aca459f3369cdac4a335
YingHao <yinghao.developer@gmail.com>
2016-04-01 12:25:18 +0800

    ReservationController updated

--- OODP Assignment/src/controller/hrs/ReservationController.java
+++ OODP Assignment/src/controller/hrs/ReservationController.java
@@ -1,9 +1,31 @@
 package controller.hrs;
 
+import java.util.List;
+
+import controller.PersistenceController;
+import persistence.Persistence;
+import view.View;
+
 /**
- * 
+ * ReservationController is a controller that performs reservation operations.
  * @author YingHao
  */
-public class ReservationController {
+public class ReservationController extends PersistenceController {
+
+	public ReservationController(Persistence persistence) {
+		super(persistence);
+	}
+
+	@Override
+	public List<String> getOptions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void safeOnOptionSelected(View view, int option) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
