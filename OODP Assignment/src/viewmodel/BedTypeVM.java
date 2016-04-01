a86f222f05ede07ff6d01ae62ddfe07e5fe2f75f
YingHao <yinghao.developer@gmail.com>
2016-04-02 00:14:14 +0800

    Added ViewModels for Reservation

---
+++ OODP Assignment/src/viewmodel/BedTypeVM.java
@@ -0,0 +1,46 @@
+package viewmodel;
+
+import model.Room;
+import model.BedType;
+
+/**
+ * A view model class that encapsulates information about a {@link BedType} and the number of {@link Room} instances with the
+ * specified {@link BedType}.
+ * @author YingHao
+ */
+public class BedTypeVM {
+	private final BedType bedType;
+	private final long count;
+	
+	/**
+	 * BedTypeVM constructor.
+	 * @param bedType - BedType that is associated to this instance.
+	 * @param count - The number of rooms that are available for the associated bed type.
+	 */
+	public BedTypeVM(BedType bedType, long count) {
+		this.bedType = bedType;
+		this.count = count;
+	}
+
+	/**
+	 * Gets the {@link BedType} associated with this instance.
+	 * @return bedType
+	 */
+	public BedType getBedType() {
+		return bedType;
+	}
+	
+	/**
+	 * Gets the number of {@link Room} instances that are available with the specified {@link BedType}.
+	 * @return count
+	 */
+	public long getCount() {
+		return count;
+	}
+
+	@Override
+	public String toString() {
+		return bedType.toString() + "(" + count + " room(s) available)";
+	}
+	
+}
---
+++ OODP Assignment/src/viewmodel/TextAndCountVM.java
@@ -0,0 +1,55 @@
+package viewmodel;
+
+/**
+ * A view model class that encapsulates information about a text and a count.
+ * @author YingHao
+ */
+public class TextAndCountVM {
+	private final String text;
+	private long count;
+	
+	/**
+	 * TextAndCountVM constructor.
+	 * @param text - The text that is associated to this instance.
+	 * @param count - The count that is associated to this instance.
+	 */
+	public TextAndCountVM(String text, long count) {
+		this.text = text;
+		this.count = count;
+	}
+	
+	/**
+	 * Gets the text associated with this instance.
+	 * @return view
+	 */
+	public String getText() {
+		return text;
+	}
+	
+	/**
+	 * Gets the count.
+	 * @return count
+	 */
+	public long getCount() {
+		return count;
+	}
+
+	/**
+	 * Sets the count.
+	 * @param count
+	 */
+	public void setCount(long count) {
+		this.count = count;
+	}
+	
+	@Override
+	public String toString() {
+		return text + "(" + count + " room(s) available)";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return (obj instanceof TextAndCountVM) && ((TextAndCountVM)obj).getText().equals(this.getText());
+	}
+
+}
