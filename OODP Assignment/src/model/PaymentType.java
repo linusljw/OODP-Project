d25e294bee5a61eab3fb05fff8ad6a833d9aa6d1
YingHao <yinghao.developer@gmail.com>
2016-04-02 16:42:25 +0800

    Added PaymentType for reservation to indicate payment type of course

---
+++ OODP Assignment/src/model/PaymentType.java
@@ -0,0 +1,19 @@
+package model;
+
+/**
+ * PaymentType is an enumeration that specifies the possible payment types.
+ * @author YingHao
+ */
+public enum PaymentType {
+	
+	/**
+	 * Cash payment.
+	 */
+	Cash,
+	
+	/**
+	 * Credit card payment.
+	 */
+	CreditCard;
+
+}
