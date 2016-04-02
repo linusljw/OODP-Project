685294afc2e9c15a1859fbea134139f2b38c445e
YingHao <yinghao.developer@gmail.com>
2016-04-02 20:40:12 +0800

    Commit new CheckInPredicate class

---
+++ OODP Assignment/src/controller/hrs/CheckInPredicate.java
@@ -0,0 +1,37 @@
+package controller.hrs;
+
+import java.util.Date;
+
+import model.Guest;
+import model.Reservation;
+import model.ReservationStatus;
+import persistence.Predicate;
+
+/**
+ * CheckInPredicate is a predicate class that determines whether a Reservation
+ * passes or fails a check-in predicate which includes conditions such as start and end date and
+ * guest it belongs to.
+ * @author YingHao
+ */
+public class CheckInPredicate implements Predicate<Reservation> {
+	private final Guest guest;
+	
+	/**
+	 * CheckInPredicate constructor.
+	 * @param guest - The guest to perform this check in predicate on.
+	 */
+	public CheckInPredicate(Guest guest) {
+		this.guest = guest;
+	}
+
+	@Override
+	public boolean test(Reservation item) {
+		Date today = new Date();
+		
+		return (item.getStatus() == ReservationStatus.Confirmed || item.getStatus() == ReservationStatus.Waitlist) &&
+				item.getGuest().equals(guest) &&
+				!today.before(item.getStartDate()) &&
+				!today.after(item.getEndDate());
+	}
+
+}
